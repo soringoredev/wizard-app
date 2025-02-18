7ec933456d594b3cdf1eea1e46d3a7d6f3ccd5c8
Sorin Gore <soringoredev@gmail.com>
2025-02-18 18:53:02 +0200

    Initial commit.

---
+++ src/main/java/pixel/academy/wizard_app/WizardAppApplication.java
@@ -0,0 +1,13 @@
+package pixel.academy.wizard_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WizardAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WizardAppApplication.class, args);
+	}
+
+}
